ada01dbedf6d855900cd09a6f42caff67c166ff1
prashan7070 <prashan7070@gmail.com>
2025-12-11 09:40:49 +0530

    auth coontext added

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useContext, useState, useEffect, type  ReactNode } from 'react';
+import type { User } from '../types/user.types';
+import { authService } from '../services/auth.service';
+
+interface AuthContextType {
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (user: User, accessToken: string, refreshToken: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // Initialize from LocalStorage on mount
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    const token = localStorage.getItem('accessToken');
+    if (storedUser && token) {
+      setUser(JSON.parse(storedUser));
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const login = (userData: User, accessToken: string, refreshToken: string) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    setUser(userData);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    setUser(null);
+    setIsAuthenticated(false);
+    authService.logout();
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom Hook for ease of use
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
